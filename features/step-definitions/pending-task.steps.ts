190773af441daedd1d80eb234a2c5dfd322c95b2
Crys <crysthel.aparicio@trebol-it.com>
2024-01-05 01:31:52 -0400

    improvement

--- features/step-definitions/pending-task.steps.ts
+++ features/step-definitions/pending-task.steps.ts
@@ -1,15 +1,12 @@
-import { Given, When, Then } from '@wdio/cucumber-framework';
+import { When, Then } from '@wdio/cucumber-framework';
 import PendingTaskPage from '../pageobjects/pending-task.page.js';
 
-Given("I am on the login page", async (page) => {
-    ////
- });
 
-When("The user selects the first checkbox for a task to complete it", async () => {
+When(/^The user selects the first checkbox for a task to complete it$/, async () => {
     await PendingTaskPage.clickCheckTask();
 });
 
-Then("The user sees the name of the underlined task and displayed in the completed list", async () => {
+Then(/^The user sees the name of the underlined task and displayed in the completed list$/, async () => {
     await PendingTaskPage.assertionCheckTaskSelected();
 });
 
